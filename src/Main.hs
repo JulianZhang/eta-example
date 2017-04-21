67538ad8ad0fe3c37da1428e8270b2bd6e1ad5e9
zhangjun <zhangjun.julian@gmail.com>
2017-04-21 13:29:51 +0800

    work able for options

--- src/Main.hs
+++ src/Main.hs
@@ -1,10 +1,27 @@
 {-# LANGUAGE MagicHash #-}
 
-import Java
+-- import Java
+import Control.Applicative
+import Options
 
+data MainOptions = MainOptions
+	{ port :: Int
+	, message :: String
+	}
 
-foreign import java unsafe "@static eta.dl4j.Word2VecRawText.mainfun"
-  word2VecRawText :: String->String -> IO ()
+instance Options MainOptions where
+    	defineOptions = pure MainOptions
+    		<*> simpleOption "port" 8088
+    		    "the port for web service "
+    		<*> simpleOption "message" "test message"
+    		    "message for testing."
+
+--foreign import java unsafe "@static eta.dl4j.Word2VecRawText.mainfun"
+--  word2VecRawText :: String->String -> IO ()
 
 main :: IO ()
-main = word2VecRawText "/Users/zhangjun/Documents/fudan/paper/run/logfile/Cods.log.2015-01-25" "/Users/zhangjun/code/ibm/eta-example/testoutput"
+main = runCommand $ \opts args -> do
+		putStrLn (message opts)
+
+
+    --word2VecRawText "/Users/zhangjun/Documents/fudan/paper/run/logfile/Cods.log.2015-01-25" "/Users/zhangjun/code/ibm/eta-example/testoutput"
