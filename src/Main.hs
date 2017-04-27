d48f2ca69e0bd09d88d3d2f91d27ccc003cc7872
zhangjun <zhangjun.julian@gmail.com>
2017-04-27 22:00:34 +0800

    for test

--- src/Main.hs
+++ src/Main.hs
@@ -1,9 +1,45 @@
 {-# LANGUAGE MagicHash #-}
+{-# LANGUAGE DataKinds       #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TypeOperators   #-}
+
 
 import Java
 import Control.Applicative
 import Options
 
+import Data.Aeson
+import Data.Aeson.TH
+import Network.Wai
+import Network.Wai.Handler.Warp
+import Servant
+
+data User = User
+  { userId        :: Int
+  , userFirstName :: String
+  , userLastName  :: String
+  } deriving (Eq, Show)
+
+$(deriveJSON defaultOptions ''User)
+
+type API = "users" :> Get '[JSON] [User]
+
+startApp :: IO ()
+startApp = run 8080 app
+
+app :: Application
+app = serve api server
+
+api :: Proxy API
+api = Proxy
+
+server :: Server API
+server = return users
+
+users :: [User]
+users = [ User 1 "Isaac" "Newton"
+        , User 2 "Albert" "Einstein"
+        ]
 
 
 data MainOptions = MainOptions
@@ -21,7 +57,10 @@ instance Options MainOptions where
 foreign import java unsafe "@static eta.dl4j.Word2VecRawText.mainfun"
   word2VecRawText :: String->String -> IO ()
 
-main :: IO ()
-main = runCommand $ \opts args -> do
+
+main = startApp
+
+main1 :: IO ()
+main1 = runCommand $ \opts args -> do
 		word2VecRawText "/Users/zhangjun/Documents/fudan/paper/run/logfile/Cods.log.2015-01-25" "/Users/zhangjun/code/ibm/eta-example/testoutput"
 		print $ message opts
